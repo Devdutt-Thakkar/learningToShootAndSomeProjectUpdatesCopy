63b54765b69ad7bb2b5589c19608af29823de82a
Devdutt-Thakkar <146790291+Devdutt-Thakkar@users.noreply.github.com>
2024-04-03 10:09:22 -0700

    included off camera indicators

---
+++ Assets/Scripts/Looper.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looper : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerr;
+    [SerializeField]
+    private GameObject enemyy;
+    [SerializeField]
+    private Vector3 playerpos;
+    [SerializeField]
+    private Vector3 enemypos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    
+}
